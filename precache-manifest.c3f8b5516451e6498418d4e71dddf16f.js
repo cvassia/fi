8bada6de5f5bf72c318353a1c37a982a780e8744
vassia <vassiachrist@mbp-vassia.localdomain>
2020-01-09 11:42:40 +0100

    Updates

---
+++ precache-manifest.c3f8b5516451e6498418d4e71dddf16f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532e0c3842cba18ab168a578fae230f4",
+    "url": "/cvassia/fi.git/index.html"
+  },
+  {
+    "revision": "71bb12ac4c6790b54ec2",
+    "url": "/cvassia/fi.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bd2cf0a684547a759921",
+    "url": "/cvassia/fi.git/static/js/2.0fc53a4f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cvassia/fi.git/static/js/2.0fc53a4f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71bb12ac4c6790b54ec2",
+    "url": "/cvassia/fi.git/static/js/main.67f454c4.chunk.js"
+  },
+  {
+    "revision": "b407ae43eb7322413a01",
+    "url": "/cvassia/fi.git/static/js/runtime-main.1482d7b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cvassia/fi.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
